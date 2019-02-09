d8d38f8f93b11d417479dbe389e45da7c5ce72f1
github username <github email address>
2019-02-09 11:58:46 -0500

    started frontend

---
+++ src/containers/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Navbar = (props) => {
+  return(
+    <div className={`ui inverted ${props.color} menu`}>
+        <a className="item" href={props.link} >
+          <h2 className="ui header">
+            <div className="content">{props.header}</div>
+            <div className="sub header">{props.tagline}</div>
+          </h2>
+        </a>
+    </div>
+  )
+}
+
+export default Navbar
+
+
+            // <i className={`${props.icon} icon`}></i>
+
+
+// <Link to="/about">
+//   <a className="item">
+//    <h2 className="ui header">
+//      <i className={`${props.icon} icon`}></i>
+//      <div className="content">{props.title}</div>
+//      <div className="sub header">{props.subtitle}</div>
+//    </h2>
+//  </a>
+//  </Link>
+//  <Link className="item" to="/about">About</Link>
+//  <Link className="item" to="/paintings">Paintings</Link>
