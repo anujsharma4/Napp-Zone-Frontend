66a43b73300dd4a2242ae1117b1aa73d5fa6ad19
github username <github email address>
2019-02-09 19:45:35 -0500

    merge

--- src/components/NapCard.js
+++ src/components/NapCard.js
@@ -3,13 +3,23 @@ import React from 'react'
 const NapCard = (props) => {
   console.log(props.onSelectNap)
   return (
-    <div className="ui card">
-      <h3>{props.napObj.name}</h3>
-      <img src={props.napObj.picture} alt="logo" />
-      <h5>Location: {props.napObj.location}</h5>
-      <button data-nap-id={props.napObj.id}>More Info</button>
-      <p>Description: {props.napObj.description}</p>
+
+      <div className="ui card">
+          <div className="image">
+            <img src={props.napObj.picture} alt="logo" />
+          </div>
+      <div className="content">
+        <div className="header">
+          <h3>{props.napObj.name}</h3>
+        </div>
+        <div className="meta text-wrap">
+          <h5>Location: {props.napObj.location}</h5>
+          <button data-nap-id={props.napObj.id}>More Info</button>
+          <p>Description: {props.napObj.description}</p>
+        </div>
+      </div>
     </div>
+
   )
 }
 
