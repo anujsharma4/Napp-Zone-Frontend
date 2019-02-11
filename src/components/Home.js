bc315345aad08c47ddd98aa3d6f932ed666e7858
github username <github email address>
2019-02-11 10:59:30 -0500

    homepage styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Header } from 'semantic-ui-react'
+import logo from '../images/Logo.png'
+
 
 const Home = (props) => {
   return(
-    <div>
+    <div className="homepic">
       <Header as='h1' icon='bed' content='Home' />
+      <img alt="logo" src={logo}/>
+        <video className="background-video" loop muted autoplay playsinline poster= "https://media.giphy.com/media/GC7C2Fi902BDG/giphy.gif">
+          <source src="https://media.giphy.com/media/GC7C2Fi902BDG/giphy.mp4" type="video/mp4"/>
+        </video>
     </div>
   )
 }
